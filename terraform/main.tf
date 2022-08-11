3893755dc5523df41e68ebfce35075159fd629a3
jcorral <joc.palomares@gmail.com>
2022-08-11 11:10:53 -0700

    fix availability zone for aws subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.1.0.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "tf-example"
